abd1fe8ca7878024f491e291211b2386ea0aed5e
ncucu <ncucu.me@kakaocommerce.com>
2020-11-22 02:06:37 +0900

    CORS TEST

--- src/main/kotlin/me/june/restaurant/config/WebConfig.kt
+++ src/main/kotlin/me/june/restaurant/config/WebConfig.kt
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig: WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedMethods("*")
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedHeaders("*")
     }
 }
