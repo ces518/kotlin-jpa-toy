cd090fee490dc0e04571bff1726c60183f61f67c
ncucu <ncucu.me@kakaocommerce.com>
2020-11-28 01:29:23 +0900

    Global Parameter

--- src/main/kotlin/me/june/restaurant/config/SwaggerConfig.kt
+++ src/main/kotlin/me/june/restaurant/config/SwaggerConfig.kt
@@ -2,9 +2,11 @@ package me.june.restaurant.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import springfox.documentation.builders.ApiInfoBuilder
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.builders.*
+import springfox.documentation.schema.ModelRef
+import springfox.documentation.schema.ModelSpecification
+import springfox.documentation.schema.ScalarType
+import springfox.documentation.service.ParameterType
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -16,6 +18,17 @@ class SwaggerConfig {
     @Bean
     fun swagger(): Docket {
         return Docket(DocumentationType.OAS_30)
+                .globalRequestParameters(
+                        listOf(
+                                RequestParameterBuilder()
+                                        .name("dummy-auth-id")
+                                        .description("Dummy Auth")
+                                        .`in`(ParameterType.HEADER)
+                                        .required(false)
+                                        .build()
+
+                        )
+                )
                 .apiInfo(swaggerInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
