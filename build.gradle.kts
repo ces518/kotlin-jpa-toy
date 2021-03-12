14fb0fdc28605624fbcfaf0d6c549a7f104ef684
ncucu <ncucu.me@kakaocommerce.com>
2021-03-13 02:03:59 +0900

    코틀린 Mockito 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,11 +37,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(group = "org.mockito", module = "mockito.core")
     }
     testImplementation("org.springframework.security:spring-security-test")
     implementation("org.mapstruct:mapstruct:1.4.1.Final")
@@ -64,6 +64,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
 
     implementation("io.jsonwebtoken:jjwt:0.5.1")
+
+    testImplementation ("org.mockito:mockito-inline")
 }
 
 tasks.withType<Test> {
